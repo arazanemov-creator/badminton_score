e1b63588e20bfc1c55e68f237ed27640eee71eb1
arazaneFX <hiro@niimi.net>
2026-04-03 06:11:21 +0900

    fix Service Over pronunciation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2117405147" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2530743724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
