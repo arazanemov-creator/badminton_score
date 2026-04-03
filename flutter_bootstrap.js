3541979fdc99a2786ce9ecc304e9d8ec27c865c9
arazaneFX <hiro@niimi.net>
2026-04-03 14:31:28 +0900

    fix mode toggle

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "284773712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1612438983" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
