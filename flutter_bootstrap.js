b7184727c7ddc82b3b358918d6e003f61ca0b6eb
arazaneFX <hiro@niimi.net>
2026-04-07 01:56:38 +0900

    adjust JP pitch 1.1 volume 0.85

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4237401052" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4273726837" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
