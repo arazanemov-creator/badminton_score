4e0de53edea1000b75207b577a3fb80356c81f77
arazaneFX <hiro@niimi.net>
2026-04-06 17:16:16 +0900

    serve picker, remove A+1/B+1 buttons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4148216212" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2859545017" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
