5ce0148eb1c03186b602cdcde7be7004304516ec
arazaneFX <hiro@niimi.net>
2026-05-01 18:05:13 +0900

    flash overlay on new server side too

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "351124259" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4096168803" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
